66eba8d4883a1cef3cc6ec72038bdfae4ec00eaf
Ben Biddington <ben.biddington@gmail.com>
2009-11-29 13:48:32 +0000

    Moved OAuthCredentialSet to its own file

---
+++ src/org/coriander/oauth/core/OAuthCredentialSet.scala
@@ -0,0 +1,24 @@
+package org.coriander.oauth.core
+
+final class OAuthCredentialSet (val consumer : OAuthCredential, val token : OAuthCredential) {
+    def this(consumer : OAuthCredential) {
+        this(consumer, null)
+    }
+    
+    def hasConsumer : Boolean = return consumer != null
+    def hasToken : Boolean = return token != null
+}
+
+object OAuthCredentialSet {
+    def apply(consumer : OAuthCredential) : OAuthCredentialSet  = {
+        return apply(consumer, null)
+    }
+
+    def apply(consumer : OAuthCredential, token : OAuthCredential) : OAuthCredentialSet  = {
+        return new OAuthCredentialSet(consumer, token)
+    }
+
+    def forConsumer(consumer : OAuthCredential) : OAuthCredential = consumer
+    def andToken(token : OAuthCredential) : OAuthCredential = token
+    def andNoToken : OAuthCredential = null
+}
