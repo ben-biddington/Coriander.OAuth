9178c5a5a3755ea6310cc84eb26acc651b71306f
Ben Biddington <ben.biddington@gmail.com>
2010-09-30 16:35:33 +1300

    Able to get correct signature when compared with test client.
    * See: http://term.ie/oauth/example/client.php?sig_method=RSA-SHA1

--- test/org/coriander/unit/tests/oauth/core/cryptography/RsaSha1Test.scala
+++ test/org/coriander/unit/tests/oauth/core/cryptography/RsaSha1Test.scala
@@ -5,6 +5,8 @@ import org.junit.Assert._
 import org.hamcrest.core.Is._
 import org.hamcrest.core.IsEqual._
 import java.security._
+import interfaces.RSAPrivateKey
+import spec.PKCS8EncodedKeySpec
 import org.bouncycastle.openssl.PEMReader
 import org.bouncycastle.jce.provider.{X509CertificateObject, BouncyCastleProvider}
 import java.io.{FileReader, BufferedReader}
@@ -30,6 +32,37 @@ class RsaSha1Test  {
 	    assertTrue("Certificate load failed", certificate != null)
 		assertThat(certificate.getPublicKey.getAlgorithm, is(equalTo("RSA")))
 		assertThat(certificate.getSignature.size, is(equalTo(128)))
+
+		certificate.checkValidity
+	}
+
+	@Test
+	def how_to_load_private_key_from_memory_and_then_sign_with_it {
+		val bytes = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBALRiMLAh9iimur8V" +
+		"A7qVvdqxevEuUkW4K+2KdMXmnQbG9Aa7k7eBjK1S+0LYmVjPKlJGNXHDGuy5Fw/d" +
+		"7rjVJ0BLB+ubPK8iA/Tw3hLQgXMRRGRXXCn8ikfuQfjUS1uZSatdLB81mydBETlJ" +
+		"hI6GH4twrbDJCR2Bwy/XWXgqgGRzAgMBAAECgYBYWVtleUzavkbrPjy0T5FMou8H" +
+		"X9u2AC2ry8vD/l7cqedtwMPp9k7TubgNFo+NGvKsl2ynyprOZR1xjQ7WgrgVB+mm" +
+		"uScOM/5HVceFuGRDhYTCObE+y1kxRloNYXnx3ei1zbeYLPCHdhxRYW7T0qcynNmw" +
+		"rn05/KO2RLjgQNalsQJBANeA3Q4Nugqy4QBUCEC09SqylT2K9FrrItqL2QKc9v0Z" +
+		"zO2uwllCbg0dwpVuYPYXYvikNHHg+aCWF+VXsb9rpPsCQQDWR9TT4ORdzoj+Nccn" +
+		"qkMsDmzt0EfNaAOwHOmVJ2RVBspPcxt5iN4HI7HNeG6U5YsFBb+/GZbgfBT3kpNG" +
+		"WPTpAkBI+gFhjfJvRw38n3g/+UeAkwMI2TJQS4n8+hid0uus3/zOjDySH3XHCUno" +
+		"cn1xOJAyZODBo47E+67R4jV1/gzbAkEAklJaspRPXP877NssM5nAZMU0/O/NGCZ+" +
+		"3jPgDUno6WbJn5cqm8MqWhW1xGkImgRk+fkDBquiq4gPiT898jusgQJAd5Zrr6Q8" +
+		"AO/0isr/3aa6O6NLQxISLKcPDk2NOccAfS/xOtfOz4sJYM3+Bs4Io9+dZGSDCA54" +
+		"Lw03eHTNQghS0A=="
+
+		val message = "GET&http%3A%2F%2Fxxx&oauth_consumer_key%3Dkey%26oauth_nonce%3Df5e81fc5ef498a40f57d9131ea304d2e%26oauth_signature_method%3DRSA-SHA1%26oauth_timestamp%3D1285817487%26oauth_version%3D1.0"
+		val expected = "fUl84ba3SC0IeCmuXzNP80BmRvppNXCH4wO7C4yiNXIGgXhl+skHvDT92Keu2iyiQvcZ474m2wadL5dnXQAhB13wHlUo888lOMVeL3PBMHIwmRhMjFi/8wZaH8kqQPmT5cGOF3rpWtFBKMVyIhz5qyuCd+xgPQCbv6wLsDRJeq0="
+		
+		val keyFactory : KeyFactory = KeyFactory.getInstance("RSA");
+		val privSpec : PKCS8EncodedKeySpec = new PKCS8EncodedKeySpec(decodeBase64(bytes))
+        val privKey : RSAPrivateKey = keyFactory.generatePrivate(privSpec).asInstanceOf[RSAPrivateKey]
+		
+		val sig = signCore(message.getBytes, privKey, "SHA1withRSA");
+
+		assertThat(new String(encodeBase64(sig)), is(equalTo(expected)))
 	}
 
 	@Test
