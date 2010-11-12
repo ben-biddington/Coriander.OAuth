36bafc5dde4dafecab0b9b7c498598729694576d
Ben Biddington <ben.biddington@gmail.com>
2010-11-12 14:01:40 +1300

    Improved the readme preview
    * Seealso: https://gist.github.com/627534

--- rakefile.rb
+++ rakefile.rb
@@ -1,14 +1,45 @@
 require 'rake'
 
-desc 'Generate HTML from readme'
-task :readme_preview do
-	require 'maruku'
-	
-	readme = 'README.md'
-	
-	fail("Error. There is no #{readme} in <#{pwd}>") unless File.exists?(readme)
-	
-	maruku = Maruku.new(File.read(readme), {:title => "Dynasty readme"})
+desc 'Generates github-styled html from a markdown file and writes it to stdout'
+task :preview_github_readme, :file do |_, args|
+	fail("Missing file argument") if args.file.nil?
+	fail("Missing file #{args.file}") unless File.exists?(args.file)
+	puts inject_github to_html(args.file)
+end
 
-	puts maruku.to_html_document
+def inject_github(html_text)
+	require 'nokogiri'
+
+	doc = Nokogiri::HTML(html_text)
+	body = doc.xpath('//html/body').first
+
+	readme_div = Nokogiri::XML::Element.new('div', doc) do |node|
+	   node['class'] = 'announce md'
+	   node['id'] = 'readme'
+	end
+
+	div = Nokogiri::XML::Element.new('div', doc) do |node|
+	   node['class'] = 'wikistyle'
+	end
+
+	body.children.each {|child| child.parent = div}
+	body['style'] = 'padding:20px'
+	body.add_child(readme_div)
+	readme_div.add_child div
+
+	doc.to_html
+end
+
+def to_html(markdown_file)
+	require 'maruku'
+
+	style_sheets = [
+		'http://github.com/stylesheets/bundle_common.css',
+		'http://github.com/stylesheets/bundle_github.css',
+	]
+
+	Maruku.new(File.read(markdown_file), {
+		:title => 'readme',
+		:css => style_sheets.join(' ')
+	}).to_html_document
 end
